59e8fe14b413c286cb1b1a84d9919e66a8916bdb
M@ Dunlap <mrd@vicarious.com>
2020-06-02 20:12:20 -0700

    as of running out of time

--- src/utils.rs
+++ src/utils.rs
@@ -6,13 +6,29 @@ pub fn convert_landmarks_to_dubins_path(landmarks: Vec<Landmark>) -> Vec<DubinsP
 
 pub struct SuccessorIter<'a> {
     parent: Landmark,
-    target: Point,
+    target: Landmark,
     all_nodes: &'a Vec<Circle>,
+    inner: Box<dyn Iterator<Item = Landmark> + 'a>,
 }
 
 impl<'a> SuccessorIter<'a> {
     pub fn new(parent: Landmark, target: Point, all_nodes: &Vec<Circle>) -> SuccessorIter {
-        SuccessorIter { parent, target, all_nodes }
+        let target = Landmark::Destination(target);
+        // TODO: if we want to trade RAM for CPU, sort all_nodes by deviation from the bee line to the target
+        let inner = std::iter::once(target).chain(all_nodes.iter().flat_map(|node| {
+            (0..2_i32).map(move |side| {
+                if side == 0 {
+                    Landmark::LeftOf(*node)
+                } else {
+                    Landmark::RightOf(*node)
+                }
+            })
+        }));
+        SuccessorIter { parent, target, all_nodes, inner: Box::new(inner) }
+    }
+
+    fn safe_cost_to(&self, destination: &Landmark) -> Option<i64> {
+        unimplemented!()
     }
 }
 
@@ -20,6 +36,11 @@ impl<'a> Iterator for SuccessorIter<'a> {
     type Item = (Landmark, i64);
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        while let Some(successor) = self.inner.next() {
+            if let Some(cost) = self.safe_cost_to(&successor) {
+                return Some((successor, cost));
+            }
+        }
+        None
     }
 }
